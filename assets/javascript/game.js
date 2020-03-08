ad9ea21bcbac7ba3f7dc95f0c1c6cd6461235fac
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-03-08 15:32:26 -0500

    added functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,96 @@
 // list variables
+
+var targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+var score = 0; 
+var wins = 0;
+var losses= 0;
+document.querySelector(".targetNumber").innerHTML = targetNumber
+
 var bluebtn = document.querySelector(".blue");
-bluebtn.dataset.value = 10;
+bluebtn.dataset.value = Math.floor(Math.random() * (12 - 1) + 1);
 
 bluebtn.addEventListener("click", function (event) {
     var blueVal = bluebtn.dataset.value
+    score += Number(blueVal) 
+    document.querySelector(".score").innerHTML = score
     console.log("blue")
     console.log(blueVal)
+    if (score > targetNumber) {
+        losses += 1; 
+        document.querySelector(".losses").innerHTML = losses
+
+        alert("You lost!")
+    } else if (targetNumber === score) {
+        wins += 1;
+        document.querySelector(".wins").innerHTML = wins 
+        alert("You won!")
+    }
 })
 
 var purpbtn = document.querySelector(".purple");
-purpbtn.dataset.value = Math.floor(Math.random() * 11);
+purpbtn.dataset.value = Math.floor(Math.random() * (12 - 1) + 1);
 
 purpbtn.addEventListener("click", function (event) {
     var purpleVal = purpbtn.dataset.value
+    score += Number(purpleVal) 
+    document.querySelector(".score").innerHTML = score
     console.log("purple")
     console.log(purpleVal)
+    if (score > targetNumber) {
+        losses += 1; 
+        document.querySelector(".losses").innerHTML = losses
+
+        alert("You lost!")
+    } else if (targetNumber === score) {
+        wins += 1;
+        document.querySelector(".wins").innerHTML = wins 
+        alert("You won!")
+    }
 })
 
+
 var opalbtn = document.querySelector(".opal");
-opalbtn.dataset.value = 10;
+opalbtn.dataset.value = Math.floor(Math.random() * (12 - 1) + 1);
 
 opalbtn.addEventListener("click", function (event) {
     var opalVal = opalbtn.dataset.value
+    score += Number(opalVal) 
+    document.querySelector(".score").innerHTML = score
     console.log("opal")
     console.log(opalVal)
+    if (score > targetNumber) {
+        losses += 1; 
+        document.querySelector(".losses").innerHTML = losses
+
+        alert("You lost!")
+    } else if (targetNumber === score) {
+        wins += 1;
+        document.querySelector(".wins").innerHTML = wins 
+        alert("You won!")
+    }
 })
 
+
 var pinkbtn = document.querySelector(".pink");
-pinkbtn.dataset.value = 10;
+pinkbtn.dataset.value = Math.floor(Math.random() * (12 - 1) + 1);
 
 pinkbtn.addEventListener("click", function (event) {
     var pinkVal = pinkbtn.dataset.value
+    score += Number(pinkVal) 
+    document.querySelector(".score").innerHTML = score
     console.log("pink")
     console.log(pinkVal)
+    if (score > targetNumber) {
+        losses += 1; 
+        document.querySelector(".losses").innerHTML = losses
+
+        alert("You lost!")
+    } else if (targetNumber === score) {
+        wins += 1;
+        document.querySelector(".wins").innerHTML = wins 
+        alert("You won!")
+    }
 })
 
 
-//functions
 
-//call functions
-//click
